ffd4b794c3978d55708f40332af5b553228f994e
enzoesper <enzoesper@hotmail.com>
2021-06-23 11:42:48 -0300

    reusing code with beforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import App from 'components/App';
+import App from '../App';
 import { shallow } from 'enzyme';
-import CommentBox from 'components/CommentBox';
-import CommentList from 'components/CommentList';
+import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 
 // it('shows a comment box', () => {
 //   const div = document.createElement('div');
@@ -16,15 +16,15 @@ import CommentList from 'components/CommentList';
 //   // cleanup
 //   ReactDOM.unmountComponentAtNode(div);
 // });
+let wrapped;
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
 it('shows a comment box', () => {
-  const wrapped = shallow(<App />);
-
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it('shows a comment list', () => {
-  const wrapped = shallow(<App />);
-
   expect(wrapped.find(CommentList).length).toEqual(1);
 });
