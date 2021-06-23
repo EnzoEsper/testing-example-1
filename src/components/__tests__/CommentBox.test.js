f6e125f8733add3b1e1d48c6622c72299ac747c7
enzoesper <enzoesper@hotmail.com>
2021-06-23 12:19:13 -0300

    asserting elements existence inside the CommentBox component

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CommentBox from '../CommentBox';
+import { mount } from 'enzyme';
+
+it('has a text area and a button', () => {
+  const wrapped = mount(<CommentBox />);
+
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
+});
