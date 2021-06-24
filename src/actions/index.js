07d4f0d9514d88ae01ba2b4707e5e96dd2bd51be
enzoesper <enzoesper@hotmail.com>
2021-06-24 10:32:44 -0300

    parsing and showing the comments obtained from the remote API

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,6 +11,7 @@ export function saveComment(comment) {
 export function fetchComments() {
   const response = axios.get(`http://jsonplaceholder.typicode.com/comments`);
 
+  console.log(response);
   return {
     type: FETCH_COMMENTS,
     payload: response
